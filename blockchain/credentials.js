bf3834acce44aa68ed1731b8131b1168f751c950
Sumit Guha <sumit03guha@gmail.com>
2021-08-14 02:17:42 +0530

    Final Commit

--- blockchain/credentials.js
+++ blockchain/credentials.js
@@ -1,7 +1,7 @@
 const credentials = {
   url: '', // ADD THE RINKEBY TEST NETWORK URL OBTAINED AFTER SIGNING UP AT https://infura.io/
   mnemonicPhrase: '', // ADD THE 'Secret Recovery Phrase' OBTAINED FROM YOUR METAMASK ACCOUNT
-  contractAddress: '', // ADD THE CONTRACT ADDRESS OBTAINED FROM THE TERMINAL AFTER RUNNING THE deploy.js
+  contractAddress: '', // ADD THE CONTRACT ADDRESS OBTAINED FROM THE TERMINAL AFTER RUNNING THE deploy.js {TO BE MODIFIED ONLY AFTER RUNNING THE deploy.js}
 };
 
 export default credentials;
